ad9eca3e270e1f361e755a57ad5caac271d05103
sde-sant <sde-sant@student.42madrid.com>
2022-12-05 14:33:35 +0100

    Actualizado libft.h y añadido strlcpy.c

--- 42cursus/Libft/libft.h
+++ 42cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: sde-sant <sde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 19:53:08 by sde-sant          #+#    #+#             */
-/*   Updated: 2022/09/22 20:56:59 by sde-sant         ###   ########.fr       */
+/*   Updated: 2022/12/05 14:33:02 by sde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,21 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *s1);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);				// sustituir el *restrict
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
+size_t	ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize);		// sustituir el *restrict
 size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t size);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size);
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t size);			// sustituir el *restrict
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t size);			// sustituir el *restrict
 
 #endif
