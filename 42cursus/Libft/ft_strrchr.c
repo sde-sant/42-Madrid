efc44471fc7d1119903d2c8c9cb082c0423d3b19
sde-sant <sde-sant@student.42madrid.com>
2022-12-15 21:57:19 +0100

    ft_strrchr.c en desarrollo

--- 42cursus/Libft/ft_strrchr.c
+++ 42cursus/Libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: sde-sant <sde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 10:37:47 by sde-sant          #+#    #+#             */
-/*   Updated: 2022/12/15 14:22:47 by sde-sant         ###   ########.fr       */
+/*   Updated: 2022/12/15 20:34:40 by sde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,23 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	// char	chr;
+	int		i;
 
 	i = sizeof(*s);
 	while ((s[i] != c) || i >= 0)
-	{
 		i--;
-	}
 	if (i < 0)
+		return (NULL);
+	else
 	{
-		return NULL;
+		
 	}
 }
 
-// int main(void)
-// {
-//   char buf[40] = "Hello World!";
-//   char * ptr;
-//   int    ch = 'p';
- 
-//   /* This illustrates strchr */
-//   ptr = strchr( buf, ch );
-//   printf( "The first occurrence of %c in '%s' is '%s'\n", ch, buf, ptr );
-//   printf("%s\n", ptr);
- 
-//   /* This illustrates strrchr */
-//   ptr = strrchr( buf, ch );
-//   printf( "The last occurrence of %c in '%s' is '%s'\n", ch, buf, ptr );
-//   printf("%s", ptr);
-// }
+int	main(void)
+{
+	char	s[]="Hello World!";
+	printf("%lu", sizeof(s));
+	ft_strrchr(s, 'p');
+}
